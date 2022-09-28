9d118383a4bf7e126e0f219e4c3a9df5f8c4df33
nen9mA0 <nen9ma0@outlook.com>
2022-09-28 16:14:44 +0800

    add arch, change hash

--- load.py
+++ load.py
@@ -5,6 +5,7 @@ import pickle
 import capstone
 
 suffix = ".asmdump"
+debug = True
 
 class NGramDict(object):
     def __init__(self):
@@ -27,18 +28,23 @@ class NGramSlice(object):
         self.length = ngram
         # self.mnemonic_map = {}
 
-    def Slicer(self, insn_lst):
+    def Slicer(self, insn_lst, debug=False):
         for i in range(len(insn_lst)-self.length+1):
             insn_hash, slice_bytes = self.Hash(insn_lst[i:i+self.length])
             # insn_hash = int.from_bytes(insn_hash, "little")
-            if not insn_hash in self.ngd:
-                # self.ngd[insn_hash] = slice_bytes
-                self.ngd[insn_hash] = 1
+            if debug:
+                if not insn_hash in self.ngd:
+                    self.ngd[insn_hash] = (1, [slice_bytes])
+                else:
+                    n, old_lst = self.ngd[insn_hash]
+                    n += 1
+                    old_lst.append(slice_bytes)
+                    self.ngd[insn_hash] = (n, old_lst) 
             else:
-                self.ngd[insn_hash] += 1
-                # if slice_bytes != self.ngd[insn_hash]:
-                #     raise ValueError("")
-                # self.ngd[insn_hash] += slice_bytes
+                if not insn_hash in self.ngd:
+                    self.ngd[insn_hash] = 1
+                else:
+                    self.ngd[insn_hash] += 1
             self.total += 1
         return self.ngd
 
@@ -50,6 +56,9 @@ class NGramSlice(object):
                 checksum = (checksum + byte) % 256
         return checksum 
 
+    # 旧的哈希格式，存在几个问题：
+    #   * 无法确定reg字段是否真的作为opcode
+    #   * 实践中发现有部分指令使用了整个modrm字段来区分指令，即prefix/opcode/reg是完全相同的，仅mod或rm是不同的，如vmx系列指令
     # hashing  now use type 1 for speed
     # type1   with prefix
     #  -------------------- 1 Byte --------------------- ------- 2 Byte -------                     ------------------ Last Byte ------------------
@@ -59,13 +68,84 @@ class NGramSlice(object):
     #  -------------------- 1 Byte ---------------------                   -------------------- Last Byte --------------------
     # | 1bit | 4bit                    | 3bit           | nbit            | 2bit          | 2bit      | 2bit      | 2bit      |
     # |   1  | length of prefix+opcode | reg            | prefix + opcode | operand_num   | op1 type  | op2 type  | op3-type  |
+    # def Hash(self, slice):
+    #     myhash = []
+    #     # checksum = self.HashBytes(slice)
+
+    #     # for debug
+    #     slice_bytes = b""
+    #     for insn in slice:
+    #         opcode_size = 1             # for add 
+    #                                     # 00 /r	ADD r/m8, r8	MR	Valid	Valid	Add r8 to r/m8.
+    #         for i in range(len(insn.opcode)-1, -1, -1):
+    #             if insn.opcode[i] != 0:
+    #                 opcode_size = i+1
+    #                 break
+    #         prefix_group = 0
+    #         prefix_size = 0
+    #         for i in range(len(insn.prefix)):
+    #             if insn.prefix[i] != 0:
+    #                 prefix_group |= 1<<i
+    #                 prefix_size += 1
+    #         opfix_size = opcode_size + prefix_size
+    #         insn_size = len(insn.bytes)
+    #         hash_type = 0               # here we use hash type 1
+    #         insn_size |= (hash_type << 4)
+
+    #         myhash.append( (insn_size<<3) | opfix_size )
+
+    #         reg = (insn.modrm >> 3) & 0x7
+    #         myhash.append(prefix_group << 3 | reg)
+
+    #         if prefix_size > 0:
+    #             for i in range(len(insn.prefix)):
+    #                 if insn.prefix[i] != 0:
+    #                     myhash.append(insn.prefix[i])
+    #         myhash.extend(insn.opcode[:opcode_size])
+
+    #         ops = 0
+    #         op_num = 0
+    #         for i in insn.operands:
+    #             ops = ops << 2
+    #             op_num += 1
+    #             if i.type == capstone.x86.X86_OP_REG:
+    #                 op_type = 1
+    #             elif i.type == capstone.x86.X86_OP_MEM:
+    #                 op_type = 2
+    #             elif i.type == capstone.x86.X86_OP_IMM:
+    #                 op_type = 3
+    #             else:
+    #                 raise ValueError("")
+    #             ops |= op_type
+    #         if op_num > 4:
+    #             raise ValueError("")
+
+    #         for num in range(op_num, 4):
+    #             ops = ops << 2
+
+    #         myhash.append(ops)
+
+    #         # for debug
+    #         slice_bytes += insn.bytes
+
+    #     return bytes(myhash), slice_bytes
+
+
+    # 新hash格式
+    # * 直接在最后加上mnemonic，这样直接不用modrm这位了（说实话我觉得这个解决方案很ugly，但是先将就着用吧，总比再解析XED规则每次都放到一个大表里比较要好）
+    #  -------------------- 1 Byte --------------------- ------------- 1 Byte -------------- ----- n Byte ------ -------------------- 1 Byte ------------------- -- [optional] n Byte --
+    # | 1bit | 4bit           | 3bit                    |     4bit     |        4bit        |        nbit       | 2bit      | 2bit      | 2bit      | 2bit      |         n bit         |
+    # |   0  | length of insn | length of prefix+opcode | prefix group | length of mnemonic |   prefix + opcode | op1 type  | op2 type  | op3 type  | op4 type  |        mnemonic       |
+
     def Hash(self, slice):
-        myhash = []
+        ret = b""
         # checksum = self.HashBytes(slice)
 
         # for debug
         slice_bytes = b""
+
         for insn in slice:
+            myhash = []
             opcode_size = 1             # for add 
                                         # 00 /r	ADD r/m8, r8	MR	Valid	Valid	Add r8 to r/m8.
             for i in range(len(insn.opcode)-1, -1, -1):
@@ -80,13 +160,16 @@ class NGramSlice(object):
                     prefix_size += 1
             opfix_size = opcode_size + prefix_size
             insn_size = len(insn.bytes)
-            hash_type = 0               # here we use hash type 1
-            insn_size |= (hash_type << 4)
+            hash_type = 0
+            tmp_byte = (hash_type << 4) | insn_size
 
-            myhash.append( (insn_size<<3) | opfix_size )
+            myhash.append( (tmp_byte<<3) | opfix_size )
 
-            reg = (insn.modrm >> 3) & 0x7
-            myhash.append(prefix_group << 3 | reg)
+            mnemonic_len = len(insn.mnemonic)
+            if mnemonic_len > 15:
+                raise ValueError("mnemonic length larger than 15")
+            tmp_byte = (prefix_group << 4) | mnemonic_len
+            myhash.append(tmp_byte)
 
             if prefix_size > 0:
                 for i in range(len(insn.prefix)):
@@ -113,13 +196,17 @@ class NGramSlice(object):
 
             for num in range(op_num, 4):
                 ops = ops << 2
-
             myhash.append(ops)
+
+            tmp_byte = bytes(insn.mnemonic, "ascii")
+            if len(tmp_byte) != mnemonic_len:
+                raise ValueError("Length different after encode")
+            ret += bytes(myhash) + tmp_byte
 
             # for debug
             slice_bytes += insn.bytes
 
-        return bytes(myhash), slice_bytes
+        return ret, slice_bytes
 
 
 
@@ -198,6 +285,7 @@ if __name__ == "__main__":
                 if arch in tmp:
                     flag = True
             if not flag:
+                print("file: %s not in allow arch" %file)
                 continue
 
         func_bytes = ConcatBytes(disasm_dict["disasm"])
@@ -208,7 +296,7 @@ if __name__ == "__main__":
             decode = cs.disasm(func_bytes[addr], baddr)
             for insn in decode:
                 insn_lst.append(insn)
-            ngs.Slicer(insn_lst)
+            ngs.Slicer(insn_lst, debug)
         file_num += 1
 
     database = {}
--- ngram_reader.py
+++ ngram_reader.py
@@ -6,7 +6,11 @@ import pickle
 import capstone
 
 cs = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
+debug = True
+if debug:
+    cs.detail = True
 
+# [Duplicate] old format, see comment in load.py
 # hashing  now use type 1 for speed
 # type1   with prefix
 #  -------------------- 1 Byte --------------------- ------- 2 Byte -------                     -------------------- Last Byte --------------------
@@ -16,11 +20,74 @@ cs = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
 #  -------------------- 1 Byte ---------------------                   -------------------- Last Byte --------------------
 # | 1bit | 4bit                    | 3bit           | nbit            | 2bit          | 2bit      | 2bit      | 2bit      |
 # |   1  | length of prefix+opcode | reg            | prefix + opcode | operand_num   | op1 type  | op2 type  | op3-type  |
+# def ParseSlice(slice):
+#     end = len(slice)
+#     i = 0
+#     begin = 0
+#     while i < end:
+#         begin = i
+#         disasm_lst = []
+#         insn_size = (slice[i] >> 3) & 0xf
+#         opfix_size = slice[i] & 0x7
+#         i += 1
 
-def ParseSlice(slice):
+#         prefix_group = (slice[i] >> 3) & 0x1f
+#         reg = slice[i] & 0x7
+#         i += 1
+
+#         disasm_lst.extend(slice[i:i+opfix_size])
+#         i += opfix_size
+
+#         if insn_size-opfix_size > 0:
+#             disasm_lst.append(reg<<3)       # push modrm
+#             for j in range(insn_size-opfix_size-1):
+#                 disasm_lst.append(0)
+
+#         # operand
+#         ops = slice[i]
+#         # op_num = ops >> 6
+#         op_types = []
+#         for j in range(4):
+#             if ops & 0x3 != 0:
+#                 op_type = ops&0x3
+#                 if op_type == 1:
+#                     op_types.append("reg")
+#                 elif op_type == 2:
+#                     op_types.append("mem")
+#                 elif op_type == 3:
+#                     op_types.append("imm")
+#             else:
+#                 if len(op_types) > 0:
+#                     raise ValueError("")
+#             ops = ops >> 2
+#         i += 1
+
+#         disasm_code = bytes(disasm_lst)
+#         decode = cs.disasm(disasm_code, 0)
+#         num = 0
+#         flag = False
+#         for insn in decode:
+#             flag = True
+#             if num != 0:
+#                 break
+#             mystr = insn.mnemonic + " "
+#             mystr += " ".join(op_types[::-1])
+#             print(mystr)
+#             num += 1
+#         if not flag:
+#             print("%s :  Disassemble Error" %slice[begin:i].hex())
+#     if i != end:
+#         raise ValueError("%s : Parsing Result Not Equal" %slice[begin:i].hex())
+
+# New format
+#  -------------------- 1 Byte --------------------- ------------- 1 Byte -------------- ----- n Byte ------ -------------------- 1 Byte ------------------- -- [optional] n Byte --
+# | 1bit | 4bit           | 3bit                    |     4bit     |        4bit        |        nbit       | 2bit      | 2bit      | 2bit      | 2bit      |         n bit         |
+# |   0  | length of insn | length of prefix+opcode | prefix group | length of mnemonic |   prefix + opcode | op1 type  | op2 type  | op3 type  | op4 type  |        mnemonic       |
+def ParseSlice(slice, debug=False, slice_bytes=None, slient_check=False):
     end = len(slice)
     i = 0
     begin = 0
+    result = []
     while i < end:
         begin = i
         disasm_lst = []
@@ -28,17 +95,12 @@ def ParseSlice(slice):
         opfix_size = slice[i] & 0x7
         i += 1
 
-        prefix_group = (slice[i] >> 3) & 0x1f
-        reg = slice[i] & 0x7
+        mnemonic_len = slice[i] & 0x0f
+        prefix_group = slice[i] >> 4
         i += 1
 
         disasm_lst.extend(slice[i:i+opfix_size])
         i += opfix_size
-
-        if insn_size-opfix_size > 0:
-            disasm_lst.append(reg<<3)       # push modrm
-            for j in range(insn_size-opfix_size-1):
-                disasm_lst.append(0)
 
         # operand
         ops = slice[i]
@@ -59,28 +121,58 @@ def ParseSlice(slice):
             ops = ops >> 2
         i += 1
 
-        disasm_code = bytes(disasm_lst)
-        decode = cs.disasm(disasm_code, 0)
-        num = 0
-        flag = False
-        for insn in decode:
-            flag = True
-            if num != 0:
-                break
-            mystr = insn.mnemonic + " "
-            mystr += " ".join(op_types[::-1])
+        mnemonic = slice[i:i+mnemonic_len].decode("ascii")
+        i += mnemonic_len
+
+        mystr = mnemonic + " "
+        mystr += " ".join(op_types[::-1])
+        if not slient_check:
             print(mystr)
-            num += 1
-        if not flag:
-            print("%s :  Disassemble Error" %slice[begin:i].hex())
+
+        if debug:
+            result.append(mystr)
+
     if i != end:
         raise ValueError("%s : Parsing Result Not Equal" %slice[begin:i].hex())
+
+
+    if debug:
+        for slice_byte in slice_bytes:
+            decode = cs.disasm(slice_byte, 0)
+            flag = False
+            disasm_insn = []
+            disasm_str = []
+            for insn in decode:
+                flag = True
+                insn_op_type = []
+                for i in insn.operands:
+                    if i.type == capstone.x86.X86_OP_REG:
+                        insn_op_type.append("reg")
+                    elif i.type == capstone.x86.X86_OP_MEM:
+                        insn_op_type.append("mem")
+                    elif i.type == capstone.x86.X86_OP_IMM:
+                        insn_op_type.append("imm")
+                    else:
+                        raise ValueError("")
+                mystr = insn.mnemonic + " " + " ".join(insn_op_type)
+                disasm_insn.append(insn)
+                disasm_str.append(mystr)
+            if len(disasm_str) != len(result):
+                raise ValueError("")
+            for i in range(len(result)):
+                if result[i] != disasm_str[i]:
+                    raise ValueError("")
+            if not flag:
+                print("%s :  Disassemble Error" %slice[begin:i].hex())
+
+
 
 
 if __name__ == "__main__":
     opts, args = getopt.getopt(sys.argv[1:], "i:b:l:fs")
     display_file = False
-    display_size = False
+    display_size = True
+    slient_check = False
     begin = 0
     length = 0
     for opt, value in opts:
@@ -93,7 +185,7 @@ if __name__ == "__main__":
         elif opt == "-f":
             display_file = True
         elif opt == "-s":
-            display_size = True
+            slient_check = True
 
     if not os.path.exists(infile):
         raise ValueError("infile not exist: %s" %infile)
@@ -114,12 +206,14 @@ if __name__ == "__main__":
     print(total)
 
     # for debug check
-    mysum = 0
-    for i in hash_dict["data"]:
-        mysum += hash_dict["data"][i]
 
-    if mysum != total:
-        raise ValueError("")
+    if debug:
+        mysum = 0
+        for i in hash_dict["data"]:
+            mysum += hash_dict["data"][i][0]
+
+        if mysum != total:
+            raise ValueError("")
 
     num = 0
     if length == 0:
@@ -135,9 +229,16 @@ if __name__ == "__main__":
             #     mystr = insn.mnemonic + " " + insn.op_str
             #     print(mystr)
             # print("")
-            print("===== %d =====" %hash_dict["data"][slice])
-            ParseSlice(slice)
-            print("")
+
+            if not debug:
+                print("===== %d =====" %hash_dict["data"][slice])
+                ParseSlice(slice)
+            else:
+                if not slient_check:
+                    print("===== %d =====" %hash_dict["data"][slice][0])
+                ParseSlice(slice, debug=True, slice_bytes=hash_dict["data"][slice][1], slient_check=slient_check)
+            if not slient_check:
+                print("")
             num += 1
         else:
             pass
